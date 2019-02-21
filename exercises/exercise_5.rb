381ad53aa8dbcea619f4ab0d8711afd070f5b9c6
Lwong01 <leon.lo.wong@gmail.com>
2019-02-21 16:40:20 +0000

    all excercise done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "The sum of annual revenue is: $#{Store.sum("annual_revenue")}"
+
+puts "The average annual revenue is : $#{Store.average("annual_revenue")}"
